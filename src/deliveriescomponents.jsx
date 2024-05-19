ccb24c6b440d14b60cc6e31da0a13b19f9406b94
Iretunde <ppappydon007@gmail.com>
2024-05-19 10:02:33 +0100

    Push

--- src/deliveriescomponents.jsx
+++ src/deliveriescomponents.jsx
@@ -1,5 +1,6 @@
 import LogoComponent from "./Logos";
 import PageTitle from "./PageTitle.jsx";
+import Subheader from "./subheader.jsx";
 
 function DeliverablesComponent(){
     return(
@@ -7,7 +8,10 @@ function DeliverablesComponent(){
             <PageTitle text={["Boosting Business. Today",<br/>
                 , "and Tomorrow"]}/>
             <div className="deliverables">
-                
+            <PageTitle text={"Optimise expense Management as a team"}/>
+            <Subheader text={["Bring harmony to team expenses with budget limits and real-time monitiring.",<br/>
+            ,"Freedom for your staff. Peace of mind for you."]}/>
+            <a href="#" className ="Explore">Explore More</a>
             </div>
         </>
     );
