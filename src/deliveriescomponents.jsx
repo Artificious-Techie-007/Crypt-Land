eff465ff7ed6dc44c11dffdf97512393da044c53
Iretunde <ppappydon007@gmail.com>
2024-05-19 16:06:11 +0100

    Push

--- src/deliveriescomponents.jsx
+++ src/deliveriescomponents.jsx
@@ -1,17 +1,42 @@
 import LogoComponent from "./Logos";
+import deliverable1 from "./cryptassessts/deliverable1.svg";
+import deliverable2 from "./cryptassessts/deliverables2.svg";
 import PageTitle from "./PageTitle.jsx";
 import Subheader from "./subheader.jsx";
 
 function DeliverablesComponent(){
     return(
         <>
+            <div className="deliverables">
             <PageTitle text={["Boosting Business. Today",<br/>
                 , "and Tomorrow"]}/>
-            <div className="deliverables">
-            <PageTitle text={"Optimise expense Management as a team"}/>
-            <Subheader text={["Bring harmony to team expenses with budget limits and real-time monitiring.",<br/>
-            ,"Freedom for your staff. Peace of mind for you."]}/>
-            <a href="#" className ="Explore">Explore More</a>
+                <div className="deliverable1">
+                    <div className="header1">
+                        <PageTitle text={["Optimise expense",<br/>
+                        ," Management as a team"]}/>
+                        <Subheader text={["Bring harmony to team expenses with budget limits and",<br/>
+                        ," real-time monitiring,Freedom for your staff, Peace of mind",<br/>,
+                        "for you."]}/>    
+                        <a href="#" className ="Explore">Explore More</a>
+                    </div>
+                    <div className="logo">
+                        <LogoComponent logo={deliverable1} text={''}/>
+                    </div>
+                </div>
+                <div className="deliverable2">
+                    <div className="header2">
+                        <PageTitle text={["Real-time accounting",<br/>
+                        ," at your fingertips."]}/>
+                        <Subheader text={["Take the pain out of book keeping! Wave goodbye to",<br/>
+                        ," mountains of paperwork and endless email reminders",<br/>,
+                        "There`s now a new way of accounting."]}/>    
+                        <a href="#" className ="Explore2">Explore More</a>
+                    </div>
+                    <div className="logo">
+                        <LogoComponent logo={deliverable2} text={''}/>
+                    </div>
+                </div>
+                
             </div>
         </>
     );
