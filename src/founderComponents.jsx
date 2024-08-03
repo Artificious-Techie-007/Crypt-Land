692d9bc399526cebe03193e4b3b4fee7bde1db1d
Iretunde <ppappydon007@gmail.com>
2024-08-03 21:32:38 +0100

    Donee

--- src/founderComponents.jsx
+++ src/founderComponents.jsx
@@ -1,6 +1,7 @@
 import LogoComponent from "./Logos";
 import PageTitle from "./PageTitle.jsx";
 import Subheader from "./subheader.jsx";
+import ceo1 from "./cryptassessts/ceo1.svg"
 
 function FounderComponent(){
     return(
@@ -11,8 +12,8 @@ function FounderComponent(){
         <Subheader text={["we provide software for the financial and operational needs of today`s ",<br/>,
         " founders so they can get back to doing what they do best-building"]} />
 
-        <div className="ceo1">
-          <LogoComponent logo={''} text={''}/>
+        <div className="ceo1container">
+          <LogoComponent logo={ceo1} text={'Benjamin Franklin'}/>
         </div>
         </div>
 
